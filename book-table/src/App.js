049d12f1ee5d9491f5be186c608a8cf1f51e5f01
S-Dandara <daandara.s@gmail.com>
2023-06-28 14:43:16 +0100

    Move Nav from Header to App

--- book-table/src/App.js
+++ book-table/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './Header.js';
+import Nav from './Nav.js';
 import Main from './Main.js';
 import Footer from './Footer.js';
 
@@ -7,6 +8,7 @@ function App() {
   return (
   <>
     <Header />
+    <Nav />
     <Main />
     <Footer />
   </>
--- book-table/src/Header.js
+++ book-table/src/Header.js
@@ -1,11 +1,9 @@
 import logo from './assets/logo.svg';
-import Nav from './Nav.js';
 
 function Header() {
     return (
         <>
         <img src={logo} alt="Logo" />
-        <Nav />
         </>
     )
   }
